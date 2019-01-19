a1b430f5984ca53aec24ffce6256104428ec3fe6
Avery Barron <stereoalliance@gmail.com>
2019-01-18 21:17:04 -0800

    ZOMGWTFBBQ IT WORKS \!

---
+++ is/app/server/index.ts
@@ -0,0 +1,27 @@
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import * as compression from "compression";
+import * as cookieParser from "cookie-parser";
+import * as routes from "./routes";
+import nextapp from "./nextapp";
+
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+
+const app = express();
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/api", routes.api);
+app.use("/", routes.app);
+
+(async () => {
+  try {
+    await nextapp.prepare();
+    app.listen(port);
+    console.log(`server listening on port ${port}`);
+  } catch (err) {
+    console.error(err.message);
+  }
+})();
