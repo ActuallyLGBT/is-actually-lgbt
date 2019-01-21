bed9a5c41aa914b81ee9bf0c18dfe98438859c1a
Avery Barron <stereoalliance@gmail.com>
2019-01-21 13:21:01 -0800

    update codestyle in server code

--- is/app/server/routes/api/index.ts
+++ is/app/server/routes/api/index.ts
@@ -1,8 +1,8 @@
-import * as express from "express";
+import * as express from 'express';
 
 const router = express.Router();
 
-router.get("/", (_, res) => res.json({ ok: true }));
-router.get("/data", async (_, res) => res.json({ result: "foo" }));
+router.get('/', (_, res) => res.json({ ok: true }));
+router.get('/data', async (_, res) => res.json({ result: 'foo' }));
 
 export default router;
--- is/app/server/routes/app/index.ts
+++ is/app/server/routes/app/index.ts
@@ -1,17 +1,17 @@
-import * as express from "express";
-import { parse as parseUrl } from "url";
-import nextapp from "../../nextapp";
+import * as express from 'express';
+import { parse as parseUrl } from 'url';
+import nextapp from '../../nextapp';
 
 const router = express.Router();
 const handler = nextapp.getRequestHandler();
 
-router.get("/profiles/:name", (req, res) => {
+router.get('/profiles/:name', (req, res) => {
   const { name } = req.params;
 
-  nextapp.render(req, res, "/profile", { userName: name });
+  nextapp.render(req, res, '/profile', { userName: name });
 });
 
-router.get("/*", (req, res) => {
+router.get('/*', (req, res) => {
   const parsedUrl = parseUrl(req.url, true);
 
   handler(req, res, parsedUrl);
