d97b67e8dcad602ed57342a7f2f7c2b1b7484bf4
Avery Barron <stereoalliance@gmail.com>
2019-01-21 13:21:01 -0800

    update codestyle in server code

--- is/app/server/index.ts
+++ is/app/server/index.ts
@@ -1,9 +1,9 @@
-import * as express from "express";
-import * as bodyParser from "body-parser";
-import * as compression from "compression";
-import * as cookieParser from "cookie-parser";
-import * as routes from "./routes";
-import nextapp from "./nextapp";
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as compression from 'compression';
+import * as cookieParser from 'cookie-parser';
+import * as routes from './routes';
+import nextapp from './nextapp';
 
 const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
@@ -13,8 +13,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use("/api", routes.api);
-app.use("/", routes.app);
+app.use('/api', routes.api);
+app.use('/', routes.app);
 
 (async () => {
   try {
--- is/app/server/routes/index.ts
+++ is/app/server/routes/index.ts
@@ -1,2 +1,2 @@
-export { default as api } from "./api";
-export { default as app } from "./app";
+export { default as api } from './api';
+export { default as app } from './app';
